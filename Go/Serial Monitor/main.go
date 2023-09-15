ae5c6164b54e6a8ee9403690d7d3e6dc89755604
Com1 Software, Inc <information@com1software.com>
2023-09-15 10:14:46 -0400

    Update main.go

--- Go/Serial Monitor/main.go
+++ Go/Serial Monitor/main.go
@@ -60,39 +60,9 @@ func main() {
 
 			}
 
-			ch1, ch2, ch3, ch4 := getCHPosition(line)
 			fmt.Print("\033[u\033[K")
-			fmt.Printf("CH1=%s CH2=%s CH3=%s CH4=%s\n", ch1, ch2, ch3, ch4)
+			fmt.Printf(" - %s\n", line)
 
 		}
 	}
-}
-
-func getCHPosition(sentence string) (string, string, string, string) {
-	data := strings.Split(sentence, ",")
-	ch1 := ""
-	ch2 := ""
-	ch3 := ""
-	ch4 := ""
-	if len(data) == 4 {
-		ch1data := strings.Split(data[0], "=")
-		ch2data := strings.Split(data[1], "=")
-		ch3data := strings.Split(data[2], "=")
-		ch4data := strings.Split(data[3], "=")
-
-		if string(ch1data[0]) == "CH1" {
-			ch1 = ch1data[1]
-		}
-		if string(ch2data[0]) == "CH2" {
-			ch2 = ch2data[1]
-		}
-		if string(ch3data[0]) == "CH3" {
-			ch3 = ch3data[1]
-		}
-		if string(ch4data[0]) == "CH4" {
-			ch4 = ch4data[1]
-		}
-	}
-	return ch1, ch2, ch3, ch4
-
 }
